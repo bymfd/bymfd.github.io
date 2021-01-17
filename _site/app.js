e66fe09c2df2fe812d5a67919b7c6bd88e310be5
bymfd <frukolosko@gmail.com>
2021-01-17 18:36:39 +0300

    canli canli

--- _site/app.js
+++
@@ -1,1 +0,0 @@
-/* Registering Service Worker */ if('serviceWorker' in navigator) { navigator.serviceWorker.register('/https://bymfd.github.io/sw.js'); };
--- _tabs/about.md
+++
@@ -1,7 +0,0 @@
----
-title: About
-icon: fas fa-info
-order: 4
----
-
-Ben Mevlüt Furkan Demir ama siz yinede bana mfd ya da frukolosko diyebilirsiniz
